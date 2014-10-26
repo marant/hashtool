05042cd09e0de4ee5ee82e2db9ed7a649ec3a963
Marko Silokunnas <marko.silokunnas@gmail.com>
2014-10-26 15:49:34 +0200

    Add license stuff

--- hashtool.py
+++ hashtool.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python
 
+# HashTool - A simple GUI application to generate hashes for arbitrary strings
+# Copyright (C) 2014 Marko Silokunnas 
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 import wx
 import zlib
 from Crypto.Hash import MD2, MD4, MD5, SHA, SHA224, SHA256, SHA384, SHA512, RIPEMD, HMAC
