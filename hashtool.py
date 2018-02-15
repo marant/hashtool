f657d36678c27e2063f2116384d36b1a1668e4cd
Marko Silokunnas <marko.silokunnas@gmail.com>
2018-02-15 19:01:57 +0200

    Fix whitespace

--- hashtool.py
+++ hashtool.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
 
 # HashTool - A simple GUI application to generate hashes for arbitrary strings
-# Copyright (C) 2014 Marko Silokunnas 
+# Copyright (C) 2014 Marko Silokunnas
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -50,7 +50,7 @@ class Hasher:
 
     def _hashMD4(self, value, hmacKey):
         return self._cryptolibwrapper(MD4, value, hmacKey)
-    
+
     def _hashMD5(self, value, hmacKey):
         return self._cryptolibwrapper(MD5, value, hmacKey)
 
@@ -62,7 +62,7 @@ class Hasher:
 
     def _hashSHA256(self, value, hmacKey):
         return self._cryptolibwrapper(SHA256, value, hmacKey)
-    
+
     def _hashSHA384(self, value, hmacKey):
         return self._cryptolibwrapper(SHA384, value, hmacKey)
 
